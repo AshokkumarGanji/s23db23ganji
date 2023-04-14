271b672c11fdb6eee479a3d8f4edcfab3ce3bd2d
Ashok kumar Ganji <g.ashokkumar2k22@gmail.com>
2023-04-14 13:25:46 -0500

    commit after screenshot 3

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,64 +1,98 @@
 var cars = require('../models/cars');
-   
-exports.cars_list = function(req, res) {
- res.send('NOT IMPLEMENTED: cars list');
+
+exports.cars_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: cars list');
 };
 // for a specific cars.
-exports.cars_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: cars detail: ' + req.params.id);
+exports.cars_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: cars detail: ' + req.params.id);
 };
 // Handle cars create on POST.
-exports.cars_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: cars create POST');
+exports.cars_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: cars create POST');
 };
 // Handle cars delete form on DELETE.
-exports.cars_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: cars delete DELETE ' + req.params.id);
+exports.cars_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: cars delete DELETE ' + req.params.id);
 };
 // Handle cars update form on PUT.
-exports.cars_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: cars update PUT' + req.params.id);
+exports.cars_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: cars update PUT' + req.params.id);
 };
 // List of all cars
-exports.cars_list = async function(req, res) {
-    try{
-    theCars = await cars.find();
-    res.send(theCars);
+exports.cars_list = async function (req, res) {
+    try {
+        theCars = await cars.find();
+        res.send(theCars);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    } 
-   };
-   // VIEWS
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+// VIEWS
 // Handle a show all view
-exports.cars_view_all_Page = async function(req, res) {
-    try{
-    theCars = await cars.find();
-    res.render('cars', { title: 'cars Search Results', results: theCars });
+exports.cars_view_all_Page = async function (req, res) {
+    try {
+        theCars = await cars.find();
+        res.render('cars', { title: 'cars Search Results', results: theCars });
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    } 
-   };
-   // Handle Costume create on POST.
-exports.cars_create_post = async function(req, res) {
- console.log(req.body)
- let document = new cars();
- // We are looking for a body, since POST does not have query parameters.
- // Even though bodies can be in many different formats, we will be picky
- // and require that it be a json object
- // {"car_model":"Audi", "car_year":2023, "car_price":10000}
- document.car_model = req.body.car_model;
- document.car_year = req.body.car_year;
- document.car_price = req.body.car_price;
- try{
- let result = await document.save();
- res.send(result);
- }
- catch(err){
- res.status(500);
- res.send(`{"error": ${err}}`);
- } 
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+// Handle Cars create on POST.
+exports.cars_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new cars();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"car_model":"Audi", "car_year":2023, "car_price":10000}
+    document.car_model = req.body.car_model;
+    document.car_year = req.body.car_year;
+    document.car_price = req.body.car_price;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+// New code added from Assignment 12
+// for a specific Cars.
+exports.cars_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await cars.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+// Second Screenshot code
+// Handle cars update form on PUT.
+exports.cars_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await cars.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.car_model)
+            toUpdate.car_model = req.body.car_model;
+        if (req.body.car_year) toUpdate.car_year = req.body.car_year;
+        if (req.body.car_price) toUpdate.car_price = req.body.car_price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`);
+    }
 };
--- routes/cars.js
+++ routes/cars.js
@@ -4,4 +4,21 @@ var router = express.Router();
 const cars_controlers= require('../controllers/cars');
 /* GET home page. */
 router.get('/', cars_controlers.cars_view_all_Page);
+router.get('/cars/:id', cars_controlers.cars_detail);
+
+// new code implemented for  SC 2 and 3  of lab12
+
+/* GET cars */
+router.get('/',cars_controlers.cars_view_all_Page );
+router.get('/cars/:id', cars_controlers.cars_detail); 
+/* GET detail cars page */ 
+//router.get('/detail',secured, cars_controlers.cars_view_one_Page); 
+/* GET create cars page */ 
+//router.get('/create',secured, cars_controlers.cars_create_Page); 
+/* GET create update page */ 
+//router.get('/update',secured, cars_controlers.cars_update_Page); 
+/* GET delete cars page */ 
+//router.get('/delete',secured, cars_controllers.cars_delete_Page); 
+
 module.exports = router;
+
