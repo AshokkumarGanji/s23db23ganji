ad407a51b32ed6f4260e1565411dcc276bfbb751
Ashok kumar Ganji <g.ashokkumar2k22@gmail.com>
2023-04-24 02:40:41 -0500

    commit after  SS7

--- models/cars.js
+++ models/cars.js
@@ -1,8 +1,25 @@
+
+const { Double } = require("mongodb")
 const mongoose = require("mongoose")
 const carsSchema = mongoose.Schema({
-    car_model: String,
-    car_year: String,
-    car_price: Number
-})
+    // car_model: String,
+    // car_year: String,
+    // car_price: Number
+    // New code added for SS6 in Assignment13
+    car_model: {
+        type: String,
+        required: true
+    },
+    car_year: {
+        type: String,
+        required: true
+    },
+    car_price: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 50000
+    }
+});
 module.exports = mongoose.model("cars", 
 carsSchema)
--- routes/cars.js
+++ routes/cars.js
@@ -3,16 +3,26 @@ const cars_controllers= require('../controllers/cars');
 var router = express.Router();
 
 
+//new code for Assignment13
+//A little function to check if we have an authorized user and continue on or
+// redirect to login. 
+const secured = (req, res, next) => { 
+  if (req.user){ 
+    return next(); 
+  } 
+  req.session.returnTo = req.originalUrl; 
+  res.redirect("/login"); 
+} 
 
 /* GET cars */
 router.get('/',cars_controllers.cars_view_all_Page );
 router.get('/cars/:id', cars_controllers.cars_detail); 
 /* GET detail cars page */ 
-router.get('/detail',cars_controllers.cars_view_one_Page); 
+router.get('/detail',secured, cars_controllers.cars_view_one_Page); 
 /* GET create cars page */ 
-router.get('/create',cars_controllers.cars_create_Page); 
+router.get('/create',secured, cars_controllers.cars_create_Page); 
 /* GET create update page */ 
-router.get('/update',cars_controllers.cars_update_Page); 
+router.get('/update',secured, cars_controllers.cars_update_Page); 
 /* GET delete cars page */ 
-router.get('/delete',cars_controllers.cars_delete_Page); 
+router.get('/delete',secured, cars_controllers.cars_delete_Page); 
 module.exports = router;
